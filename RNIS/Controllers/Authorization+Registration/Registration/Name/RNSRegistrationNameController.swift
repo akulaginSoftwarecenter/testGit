fc5bb096fb0d2c2f19cdc0ab369b9a03926c823a
Kulagin Artem <a.kulagin@softwarecenter.ru>
2017-08-03 07:55:28 +0600

    RNSRegistrationNameController imageView

--- RNIS/Controllers/Authorization+Registration/Registration/Name/RNSRegistrationNameController.swift
+++ RNIS/Controllers/Authorization+Registration/Registration/Name/RNSRegistrationNameController.swift
@@ -11,6 +11,13 @@ import UIKit
 class RNSRegistrationNameController: UIViewController {
     
     var photo: UIImage?
+    @IBOutlet weak var imageView: UIImageView!
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        imageView.layer.cornerRadius =  imageView.frame.width/2
+    }
     
     override class var storyboardName: String {
         return "RNSRegistrationNameController"
