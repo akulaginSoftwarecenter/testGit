52690464a6ddfe23700b4616ddfdef7310e31bdd
Kulagin Artem <a.kulagin@softwarecenter.ru>
2017-08-09 12:00:53 +0600

    RNSBusStopDetailController titleLabel

--- RNIS/Controllers/Map/BusStopDetail/RNSBusStopDetailController.swift
+++ RNIS/Controllers/Map/BusStopDetail/RNSBusStopDetailController.swift
@@ -13,14 +13,17 @@ class RNSBusStopDetailController: UIViewController {
     static func initController(_ item: RNSBusStop?) -> UIViewController? {
         let vc = RNSBusStopDetailController.initialController as? RNSBusStopDetailController
         vc?.item = item
-        return STRouter.scrollShowContainer(vc, topTitle: item?.title)
+        return  STRouter.scrollShowContainer(vc, topTitle: item?.title)
     }
     
     var item: RNSBusStop?
-    @IBOutlet weak var titleLabel: RNSTopTitle!
+    
+    @IBOutlet weak var titleLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        prepareTitle()
     }
     
     func prepareTitle() {
