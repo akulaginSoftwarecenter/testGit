aadd3bc5f8035f63a59a3ef51f96c992770a00bb
Kulagin Artem <a.kulagin@softwarecenter.ru>
2017-08-29 11:46:20 +0600

    RNSRouteDetailViewController create

---
+++ RNIS/Controllers/Main/RouteDetail/RNSRouteDetailController.swift
@@ -0,0 +1,13 @@
+//
+//  RNSRouteDetailViewController.swift
+//  RNIS
+//
+//  Created by Артем Кулагин on 29.08.17.
+//  Copyright © 2017 Артем Кулагин. All rights reserved.
+//
+
+import UIKit
+
+class RNSRouteDetailViewController: UIViewController {
+
+}
