d3fbf4283c3d491f9637dd93ce05d3cb49f6e2da
Kulagin Artem <a.kulagin@softwarecenter.ru>
2017-09-11 13:53:26 +0600

    test removeOLdBusStops

--- RNIS/Managers/Map/RNSMapManager+Stub.swift
+++ RNIS/Managers/Map/RNSMapManager+Stub.swift
@@ -28,19 +28,20 @@ extension RNSMapManager {
     }
     
     static func prepareStubBusStopAsunc() {
+        removeOLdBusStops()
         RNSDataManager.createStubBusStopAsync { (items) in
             print("items")
         }
   
     }
     
-    func removeOLdBusStops() {
-        /*
+    static func removeOLdBusStops() {
+        guard let items = RNSDataManager.busStops else {
+            return
+        }
         for item in items {
             item.handlerRemove?()
-            _ = RNSPinBusStop(item)
         }
- */
     }
     
     static func prepareStubBus() {
