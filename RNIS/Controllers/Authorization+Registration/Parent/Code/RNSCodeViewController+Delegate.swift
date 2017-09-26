f8ee23dcf9dd0de52c37520ce352e291de33b11d
Kulagin Artem <a.kulagin@softwarecenter.ru>
2017-09-26 15:42:26 +0600

    RNSCodeViewController newLength <= 4

---
+++ RNIS/Controllers/Authorization+Registration/Parent/Code/RNSCodeViewController+Delegate.swift
@@ -0,0 +1,18 @@
+//
+//  RNSCodeViewController+Delegate.swift
+//  RNIS
+//
+//  Created by Артем Кулагин on 26.09.17.
+//  Copyright © 2017 Артем Кулагин. All rights reserved.
+//
+
+import Foundation
+
+extension RNSCodeViewController: UITextFieldDelegate {
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        guard let text = textField.text else { return true }
+        let newLength = text.characters.count + string.characters.count - range.length
+        return newLength <= 4 // Bool
+    }
+}
