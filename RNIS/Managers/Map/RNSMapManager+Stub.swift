adff183e923efb90b14bd1bc2b0985253e2bfbda
Kulagin Artem <a.kulagin@softwarecenter.ru>
2017-09-27 15:21:53 +0600

    RNSBusStopManager.prepareStubBusStop() remove RNSBusStopManager.prepareStubBusStop()

--- RNIS/Managers/Map/RNSMapManager+Stub.swift
+++ RNIS/Managers/Map/RNSMapManager+Stub.swift
@@ -11,7 +11,7 @@ import Foundation
 extension RNSMapManager {
     
     static func prepareStub() {
-        RNSBusStopManager.prepareStubBusStop()
+        //RNSBusStopManager.prepareStubBusStop()
         prepareStubBus()
     }
     
