dd6803b796b297a37169cdd93a919387257c7ab4
Lazarev Ivan <i_lazarev@softwarecenter.ru>
2017-09-28 16:31:47 +0300

    Implemented possibility hide zoom button on map

--- RNIS/Controllers/Settings/SettingsViewController.swift
+++ RNIS/Controllers/Settings/SettingsViewController.swift
@@ -10,6 +10,17 @@ import UIKit
 
 class SettingsViewController: UIViewController {
 
+    @IBOutlet weak var zoomButtonMap: UISwitch!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        zoomButtonMap.isOn = !UserDefaults.hideZoomButtonInMap
+    }
+
+    @IBAction func switchIsChanged(_ sender: Any) {
+        UserDefaults.hideZoomButtonInMap = !zoomButtonMap.isOn
+    }
+
     override class var storyboardName: String {
         return "SettingsViewController"
     }
