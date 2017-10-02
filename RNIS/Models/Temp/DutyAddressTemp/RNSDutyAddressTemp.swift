d7328eb46751ecf92cd3a7360f850f01cc7be100
Kulagin Artem <a.kulagin@softwarecenter.ru>
2017-10-02 08:39:35 +0600

    RNSDutyAddressTemp create

---
+++ RNIS/Models/Temp/DutyAddressTemp/RNSDutyAddressTemp.swift
@@ -0,0 +1,15 @@
+//
+//  RNSDutyAddressTemp.swift
+//  RNIS
+//
+//  Created by Артем Кулагин on 02.10.17.
+//  Copyright © 2017 Артем Кулагин. All rights reserved.
+//
+
+import UIKit
+
+class RNSDutyAddressTemp: RNISMappableBase {
+    var text: String?
+    var latitude: Double?
+    var longitude: Double?
+}
