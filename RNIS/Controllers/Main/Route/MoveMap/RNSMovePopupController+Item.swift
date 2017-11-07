222359c481493243352d4d81896c0346df685aca
Артем Кулагин <a.kulagin@softwarecenter.ru>
2017-11-07 09:43:29 +0600

    RSBSI-162 Разное поведение при завершении ведения по маршруту

--- RNIS/Controllers/Main/Route/MoveMap/RNSMovePopupController+Item.swift
+++ RNIS/Controllers/Main/Route/MoveMap/RNSMovePopupController+Item.swift
@@ -39,7 +39,7 @@ extension RNSMovePopupController {
         }
         
         alert = STRouter.showAlertOk("Маршрут завершен") {
-            STRouter.pop()
+            STRouter.popToRoot()
         }
     }
 }
