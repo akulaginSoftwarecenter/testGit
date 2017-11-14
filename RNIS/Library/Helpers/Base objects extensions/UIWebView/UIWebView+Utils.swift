8c9963231d19a6eeaa18f53f54d7e30e419c7591
Артем Кулагин <a.kulagin@softwarecenter.ru>
2017-11-14 09:02:23 +0600

    UIWebView create extension

---
+++ RNIS/Library/Helpers/Base objects extensions/UIWebView/UIWebView+Utils.swift
@@ -0,0 +1,19 @@
+//
+//  UIWebView+Utils.swift
+//  RNIS
+//
+//  Created by Артем Кулагин on 14.11.2017.
+//  Copyright © 2017 Артем Кулагин. All rights reserved.
+//
+
+import Foundation
+
+extension UIWebView {
+    func loadStubWhite(_ text: String?) {
+        guard let text = text else {
+            return
+        }
+        let page = String(format: "%@%@%@", "<div style=\"color:#ffffff\">", text, "</div>")
+        loadHTMLString(page, baseURL: nil)
+    }
+}
