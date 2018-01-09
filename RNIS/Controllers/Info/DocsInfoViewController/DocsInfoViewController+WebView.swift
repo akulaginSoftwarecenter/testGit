6da1f800dad1ddc753271aadade09e7a8c96474b
Артем Кулагин <a.kulagin@softwarecenter.ru>
2018-01-09 11:12:46 +0600

    https://regeora.myjetbrains.com/youtrack/issue/RSBSI-206
    1. Проходим по пути:Полезные контакты—-На адрес Московская область—-На сайт правительства Московской области в раздел Контактная информация. Затем я хочу выйти снова в Полезные контакты. Но я по факту нахожусь уже в них. Стрелка назад приведет меня в раздел Информация. По факту получается,что по клику на адрес Московская обл. в разделе Полезные контакты никакого перехода не произошло,а веб страница (сайт правительства МО), которая открылась находится в этом же окне.

---
+++ RNIS/Controllers/Info/DocsInfoViewController/DocsInfoViewController+WebView.swift
@@ -0,0 +1,19 @@
+//
+//  DocsInfoViewController+WebView.swift
+//  RNIS
+//
+//  Created by Артем Кулагин on 09.01.2018.
+//  Copyright © 2018 Артем Кулагин. All rights reserved.
+//
+
+import Foundation
+
+extension DocsInfoViewController: UIWebViewDelegate  {
+    func webView(_ webView: UIWebView, shouldStartLoadWith request: URLRequest, navigationType: UIWebViewNavigationType) -> Bool {
+        if navigationType == .linkClicked {
+            UIApplication.shared.openURL(request.url!)
+            return false
+        }
+        return true
+    }
+}
