6c2ec8090c935a64844b0deec9d5cb5515a2cbd4
Артем Кулагин <a.kulagin@softwarecenter.ru>
2018-01-30 15:59:45 +0600

    mobile_user_edit_source new code

--- RNIS/Models/Temp/Error/Register/RNSRegisterError.swift
+++ RNIS/Models/Temp/Error/Register/RNSRegisterError.swift
@@ -18,6 +18,7 @@ class RNSRegisterError: RNISMappableBase, RNSTextErrorProtocol {
     var favorite_path_data: [String]?
     var favorite_path_name: [String]?
     var mobile_user_email: [String]?
+    var mobile_user_edit_source: [String]?
     
     public override func mapping(map: Map) {
         
@@ -31,6 +32,7 @@ class RNSRegisterError: RNISMappableBase, RNSTextErrorProtocol {
         favorite_path_name = map.JSON["favorite_path.name"] as? [String]
         mobile_user_name = map.JSON["mobile_user.name"] as? [String]
         mobile_user_email = map.JSON["mobile_user.email"] as? [String]
+        mobile_user_edit_source = map.JSON["mobile_user.edit_source"] as? [String]
     }
     
     var textError: String? {
@@ -41,7 +43,8 @@ class RNSRegisterError: RNISMappableBase, RNSTextErrorProtocol {
                      favorite_path_data,
                      favorite_path_name,
                      mobile_user_name,
-                     mobile_user_email]
+                     mobile_user_email,
+                     mobile_user_edit_source]
         items.forEach {
             guard let text = $0?.first else {
                 return
