7fb427c7232c6f74c69fd825a0619c03873470cc
Артем Кулагин <a.kulagin@softwarecenter.ru>
2018-02-05 08:20:28 +0600

    Остановки загрузка

--- RNIS/Library/Helpers/API/Methods/POST/StopPointRoutes/RNSStopPointRoutes.swift
+++ RNIS/Library/Helpers/API/Methods/POST/StopPointRoutes/RNSStopPointRoutes.swift
@@ -9,7 +9,7 @@
 import UIKit
 import Alamofire
 
-class RNSStopPointRoutes: RNSRequest {
+class RNSStopPointRoutes: RNSPostRequestMobileToken {
     override var method: Alamofire.HTTPMethod {
         return .post
     }
