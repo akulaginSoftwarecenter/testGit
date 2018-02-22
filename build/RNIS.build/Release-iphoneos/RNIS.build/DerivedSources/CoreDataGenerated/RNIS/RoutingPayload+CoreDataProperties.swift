9e1f58271acf49fefa87da2fd1cff2bc63664f21
Артем Кулагин <a.kulagin@softwarecenter.ru>
2018-02-22 08:45:21 +0600

    new comment and settings build project

--- build/RNIS.build/Release-iphoneos/RNIS.build/DerivedSources/CoreDataGenerated/RNIS/RoutingPayload+CoreDataProperties.swift
+++ build/RNIS.build/Release-iphoneos/RNIS.build/DerivedSources/CoreDataGenerated/RNIS/RoutingPayload+CoreDataProperties.swift
@@ -2,7 +2,7 @@
 //  RoutingPayload+CoreDataProperties.swift
 //  
 //
-//  Created by Артем Кулагин on 07.02.2018.
+//  Created by Артем Кулагин on 22.02.2018.
 //
 //  This file was automatically generated and should not be edited.
 //
