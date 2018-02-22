9d8ecadec4e666736f089a0c3a92fd739495d229
Артем Кулагин <a.kulagin@softwarecenter.ru>
2018-02-22 11:05:51 +0600

    BaseViewWithXIBInit comments

--- RNIS/Library/Base classes/BaseViewWithXIBInit/BaseViewWithXIBInit.swift
+++ RNIS/Library/Base classes/BaseViewWithXIBInit/BaseViewWithXIBInit.swift
@@ -9,27 +9,36 @@
 import UIKit
 import PureLayout
 
+/**
+ Базовый класс для инициализации UIView из xib
+ */
 class BaseViewWithXIBInit: UIView {
+    
+    /// Названия xib файла
     var nibNamed:String {
         return String(describing: type(of: self))
     }
     
+    /// Метод инициализации класса
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
         
         loadNibView()
     }
     
+    /// Метод инициализации класса
     convenience init() {
         self.init(frame: CGRect.zero)
     }
     
+    /// Метод инициализации класса
     required override init(frame: CGRect) {
         super.init(frame: frame)
         
         loadNibView()
     }
     
+    /// Метод загрузки вью из xib
     func loadNibView() {
         let nibView = UINib(nibName: nibNamed, bundle: nil).instantiate(withOwner: self, options: nil)[0]
         self.insertSubview(nibView as! UIView, at: 0)
