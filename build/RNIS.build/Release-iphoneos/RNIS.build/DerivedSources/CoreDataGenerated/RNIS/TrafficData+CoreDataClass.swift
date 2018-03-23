c261cc0491a804c3bfb6bbc45647ad2dec7ed721
Артем Кулагин <a.kulagin@softwarecenter.ru>
2018-03-23 13:29:14 +0600

    Генерате док 55 процентов

--- build/RNIS.build/Release-iphoneos/RNIS.build/DerivedSources/CoreDataGenerated/RNIS/TrafficData+CoreDataClass.swift
+++ build/RNIS.build/Release-iphoneos/RNIS.build/DerivedSources/CoreDataGenerated/RNIS/TrafficData+CoreDataClass.swift
@@ -2,7 +2,7 @@
 //  TrafficData+CoreDataClass.swift
 //  
 //
-//  Created by Артем Кулагин on 22.02.2018.
+//  Created by Артем Кулагин on 23.03.2018.
 //
 //  This file was automatically generated and should not be edited.
 //
